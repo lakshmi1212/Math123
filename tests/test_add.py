88d35771845c512c8370d1fed3220a10afde85eb
lakshmi1212 <36476419+lakshmi1212@users.noreply.github.com>
2026-01-07 10:49:08 +0530

    Add or update file: test_add.py

--- tests/test_add.py
+++ tests/test_add.py
@@ -1,8 +1,15 @@
 import pytest
 from src.math_operations import add
 
-def test_add():
-    assert add(1, 2) == 3
-    assert add(-1, -1) == -2
-    assert add(0, 0) == 0
-    assert add(100, 200) == 300
+def test_add_positive_numbers():
+    assert add(2, 3) == 5
+
+def test_add_negative_numbers():
+    assert add(-2, -3) == -5
+
+def test_add_mixed_sign_numbers():
+    assert add(-2, 3) == 1
+
+def test_add_zero():
+    assert add(0, 5) == 5
+    assert add(5, 0) == 5
