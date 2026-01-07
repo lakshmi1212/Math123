d01d818312a2ba84f80400f11e3b5a255fe1c79f
lakshmi1212 <36476419+lakshmi1212@users.noreply.github.com>
2026-01-07 10:54:56 +0530

    Add or update file: test_subtract.py

--- tests/test_subtract.py
+++ tests/test_subtract.py
@@ -7,9 +7,8 @@ def test_subtract_positive_numbers():
 def test_subtract_negative_numbers():
     assert subtract(-5, -3) == -2
 
-def test_subtract_mixed_sign_numbers():
+def test_subtract_mixed_numbers():
     assert subtract(-5, 3) == -8
 
 def test_subtract_zero():
-    assert subtract(0, 5) == -5
-    assert subtract(5, 0) == 5
+    assert subtract(0, 0) == 0
